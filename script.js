bdfeb26bb946058d7d12559078077b58474cae00
Chris Williams <cwdevportfolio@gmail.com>
2026-01-19 20:00:38 -0700

    Added port key value pairs.

---
+++ script.js
@@ -0,0 +1,18 @@
+const PORTS = [
+  { port: 21, purpose: "FTP" },
+  { port: 22, purpose: "SSH" },
+  { port: 23, purpose: "Telnet" },
+  { port: 25, purpose: "SMTP" },
+  { port: 53, purpose: "DNS" },
+  { port: 80, purpose: "HTTP" },
+  { port: 143, purpose: "IMAP" },
+  { port: 161, purpose: "SNMP" },
+  { port: 443, purpose: "HTTPS" },
+  { port: 500, purpose: "IKE" },
+  { port: 587, purpose: "SMTP (Secure Submission)" },
+  { port: 990, purpose: "FTPS" },
+  { port: 993, purpose: "IMAPS (Secure IMAP)" },
+  { port: 995, purpose: "POP3S (Secure POP3)" },
+  { port: 3306, purpose: "MySQL" },
+  { port: 3389, purpose: "RDP" }
+];
